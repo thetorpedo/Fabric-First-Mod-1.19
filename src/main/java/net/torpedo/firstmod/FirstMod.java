658c0dfd6f1f83f5cfc34f416ac837e6be63163f
torpedo <thigoborges2009@gmail.com>
2023-01-17 18:47:31 -0400

    added test comment

--- src/main/java/net/torpedo/firstmod/FirstMod.java
+++ src/main/java/net/torpedo/firstmod/FirstMod.java
@@ -3,7 +3,7 @@ package net.torpedo.firstmod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//teste
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "firstmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);

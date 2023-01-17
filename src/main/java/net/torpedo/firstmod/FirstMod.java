1cc95574df3495124cc4209c270bd199e4b3fa80
torpedo <thigoborges2009@gmail.com>
2023-01-17 18:45:38 -0400

    Initial commit

---
+++ src/main/java/net/torpedo/firstmod/FirstMod.java
@@ -0,0 +1,16 @@
+package net.torpedo.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/torpedo/firstmod/FirstModClient.java
@@ -0,0 +1,10 @@
+package net.torpedo.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
